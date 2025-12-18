6715fb1fbc31b3120c76334075cef29becd86126
Your Name <luffykon8@gmail.com>
2025-12-18 17:32:46 +0700

    5th commit

--- test_app.py
+++ test_app.py
@@ -4,11 +4,7 @@ def test_home_status_code():
     response = tester.get('/users')
     assert response.status_code == 200
     assert b"Hello this is the testing API for practical exam in test_app" in response.data
-def test_home_content():
-    tester = app.test_client()
-    response = tester.get('/users')
-    assert b"Hello this is the testing API for practical exam in test_app" in response.data
+
 if __name__ == '__main__':
     app.run()
     test_home_status_code()
-    test_home_content() 
