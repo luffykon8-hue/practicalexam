3a1c051e7009c9d12bca0605b8eeea8881774fb6
Your Name <luffykon8@gmail.com>
2025-12-18 17:29:23 +0700

    3rd commit

--- test_app.py
+++ test_app.py
@@ -1,14 +1,14 @@
-from api import api
+from app import app
 def test_home_status_code():
-    tester = api.test_client()
+    tester = app.test_client()
     response = tester.get('/users')
     assert response.status_code == 200
     assert b"Hello this is the testing API for practical exam in test_app" in response.data
 def test_home_content():
-    tester = api.test_client()
+    tester = app.test_client()
     response = tester.get('/users')
     assert b"Hello this is the testing API for practical exam in test_app" in response.data
 if __name__ == '__main__':
-    api.run()
+    app.run()
     test_home_status_code()
     test_home_content() 
